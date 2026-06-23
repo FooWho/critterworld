c72dc3d71215ca713ff91d109df53f4d58c97e0b
Jason Elison <jason.elison@gmail.com>
2026-06-23 18:06:30 -0400

    More mutator

--- internal/parser/update_mutation.go
+++ internal/parser/update_mutation.go
@@ -24,9 +24,39 @@ func (m *Mutator) updateMutationRemove(locus FaultLocus) bool {
 }
 
 func (m *Mutator) updateMutationSwap(locus FaultLocus) bool {
-	return false
+	// Both sides must be MemNodes or this fails.
+	update, ok := locus.node.(*Update)
+	if !ok {
+		panic(fmt.Sprintf("critterworld: invariant violation: expected *Update in (m *Mutator).updateMutationSwap(), got %T", locus.node))
+	}
+	dest, src := update.destination, update.source
+	src, ok = src.(*MemNode)
+	if !ok {
+		return false
+	}
+	err := update.SwapChildren(dest, src)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func (m *Mutator) updateMutationReplace(locus FaultLocus) bool {
+	node, ok := locus.node.(*Update)
+	if !ok {
+		panic(fmt.Sprintf("critterworld: invariant violation: expected *Update in (m *Mutator).updateMutationReplace(), got %T", locus.node))
+	}
+	parent, ok := locus.parent.(*Rule)
+	if !ok {
+		panic(fmt.Sprintf("critterworld: invariant violation: expected *Rule in (m *Mutator).updateMutationReplace(), got %T", locus.parent))
+	}
+	candidates := GetNodesByType[*Update](&m.ast)
+	if len(candidates) < 2 {
+		return false
+	}
+	for candidate := candidates[m.rng.Intn(len(candidates))]; candidate == node; {
+		candidate = m.
+	}
+	
 	return false
 }
